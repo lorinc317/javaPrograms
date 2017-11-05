ac5e3fe8c6e6283b0bd95fe981b86043727d4b53
lorinc317 <lorinc.sandor22@gmail.com>
2017-11-05 13:22:28 +0100

    Files2 uploaded

---
+++ Files2/src/files2/Files2.java
@@ -0,0 +1,27 @@
+package files2;
+
+import java.io.*;
+public class Files2 {
+
+   public static void main(String args[]) throws IOException {
+      FileReader in = null;
+      FileWriter out = null;
+
+      try {
+         in = new FileReader("input.txt");
+         out = new FileWriter("output.txt");
+         
+         int c;
+         while ((c = in.read()) != -1) {
+            out.write(c);
+         }
+      }finally {
+         if (in != null) {
+            in.close();
+         }
+         if (out != null) {
+            out.close();
+         }
+      }
+   }
+}
